2e72cd1da087842fcae9b714f4123e31e50bcf5c
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-21 12:57:33 +0000

    start working on index.html to link to api

--- app.js
+++ app.js
@@ -22,8 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(__dirname, +"/views"));
 
-app.use('/', indexRouter);
+
+//app.use('/', indexRouter);
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
 app.use('/api/todos', todoRoutes);
 app.use('/users', usersRouter);
 
