f3b3d424f03b306e9f66155218c754777df455d6
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-22 12:21:53 +0000

    completed basic todo app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,7 +8,11 @@ $(document).ready(() => {
             createTodo();
         }
     });
-    $('.list').on('click', 'span', function() {
+    $(".list").on('click', 'li', function() {
+        updateTodo($(this));
+    });
+    $('.list').on('click', 'span', function(e) {
+        e.stopPropagation();
         removeTodo($(this).parent());
 
     });
@@ -19,7 +23,7 @@ $(document).ready(() => {
 const addTodo = (todo) => {
     let newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
     newTodo.data('id', todo._id);
-
+    newTodo.data('completed', todo.completed);
     if (todo.completed) {
         newTodo.addClass("done");
     }
@@ -55,4 +59,23 @@ const removeTodo = (todo) => {
         })
         .then(() => todo.remove())
         .catch(err => console.log(err));
-}
+};
+
+const updateTodo = (todo) => {
+    //console.log(todo.data('completed'));
+
+    var isDone = !todo.data("completed");
+    var updateData = { completed: isDone };
+    var updateUrl = '/api/todos/' + todo.data('id');
+    $.ajax({
+            method: 'PUT',
+            url: updateUrl,
+            data: updateData
+        })
+        .then(updatedTodo => {
+            todo.toggleClass("done");
+            todo.data("completed", isDone);
+            console.log(updatedTodo);
+        })
+        .catch(err => console.log(err));
+};
