19e4963db09cfb81c5b625a58ca2bc34bb9a32e4
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-23 06:27:59 +0000

    added redux for some extra practice

--- todofrontend/src/App.js
+++ todofrontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import TodoList from "./container/todolist";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This is the todoapp</h1>
+        <TodoList/>
       </div>
     );
   }
--- todofrontend/src/index.js
+++ todofrontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import TodoReducer from "./store/reducers/todoReducer";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(TodoReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
